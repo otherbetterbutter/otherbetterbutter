7afff2d223caa6191faad05d7f59c76c8c1e8350
otherbetterbutter <154960140+otherbetterbutter@users.noreply.github.com>
2023-12-29 16:54:17 +0800

    Updated video embeds with YouTube links

--- js/project3.js
+++ js/project3.js
@@ -1,4 +1,3 @@
-// Add event listener for the "Desktop" button
 document.getElementById('desktopBtn').addEventListener('click', function() {
   document.getElementById('desktopVideo').style.display = 'block';
   document.getElementById('mobileVideo').style.display = 'none';
@@ -6,10 +5,9 @@ document.getElementById('desktopBtn').addEventListener('click', function() {
   document.getElementById('mobileBtn').classList.remove('active');
 });
 
-// Add event listener for the "Mobile" button
 document.getElementById('mobileBtn').addEventListener('click', function() {
-  document.getElementById('mobileVideo').style.display = 'block';
   document.getElementById('desktopVideo').style.display = 'none';
+  document.getElementById('mobileVideo').style.display = 'block';
   this.classList.add('active');
   document.getElementById('desktopBtn').classList.remove('active');
 });
